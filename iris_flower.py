48e55924950f2c60478b68fca23b6e6dd5b685f0
AmirYousofAbdi <56346544+AmirYousofAbdi@users.noreply.github.com>
2020-07-21 15:29:58 +0430

    Update iris_flower.py

--- iris_flower.py
+++ iris_flower.py
@@ -1,25 +1,34 @@
-import pandas as pd
-
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-
-dataset = pd.read_csv('iris22.csv',names=names)
-
-x_train = []
-y_train = []
-y_test = []
-x_test = []
-n = int(input('Train data: '))
-k = 7
-
-for flower in range(150):
+def loadDataa(n):
     
-    sepal_area = dataset['sepal-width'][flower] * dataset['sepal-length'][flower]
-    petal_area = dataset['petal-width'][flower] * dataset['petal-length'][flower]
-    x_train.append([sepal_area,petal_area])
-    y_train.append(dataset['class'][flower])
 
-x_test , x_train , y_train = x_train[n:] , x_train[:n] , y_train[:n]
 
+    import pandas as pd
+
+    names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+
+    dataset = pd.read_csv('iris22.csv',names=names)
+
+    x_train = []
+    y_train = []
+    y_test = []
+    x_test = []
+
+
+
+    for flower in range(150):
+        
+        sepal_area = dataset['sepal-width'][flower] * dataset['sepal-length'][flower]
+        petal_area = dataset['petal-width'][flower] * dataset['petal-length'][flower]
+        x_train.append([sepal_area,petal_area])
+        y_train.append(dataset['class'][flower])
+
+    
+    return x_train[n:] , x_train[:n] , y_train[:n]
+
+n = int(input('Train data: '))
+
+x_test , x_train , y_train = loadData(n)
+k = 7
 difference = []
 
 for each_flower in range(len(x_test)):
@@ -50,4 +59,5 @@ for each_flower in range(len(difference)):
     
     classes.sort()
     y_test.append(classes[2][1])
+    
 print(*y_test,sep='\n')
